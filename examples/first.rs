eeb0adadd946990fc5fc99b83db8aeb8eecedfdc
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2022-10-06 00:55:49 +0300

    Negative i32

--- examples/first.rs
+++ examples/first.rs
@@ -1,29 +1,31 @@
 use std::{io::{self, Read, Stdin}, slice};
 
 fn read_i32(cin: &mut Stdin) -> io::Result<i32> {
-    let (mut b, mut v) = (0, [0u8; 10]);
+    let (mut b, mut v, mut neg) = (0, 0, None);
 
-    let mut i = 0u32;
     loop {
         cin.read(slice::from_mut(&mut b))?;
+
         if b == b' ' || b == b'\n' {
             break;
+        } else if b == b'-' && neg.is_none() {
+            neg = Some(true);
+            continue;
         } else if !(b as char).is_numeric() {
             todo!("Return an error");
         }
-        v[i as usize] = b - b'0';
-        i += 1;
+
+        v *= 10;
+        v += (b - b'0') as i32;
+
+        if v >= i32::MAX || v < i32::MIN {
+            todo!("Return an error")
+        }
     }
 
 
-    let out = v.iter()
-        .take(i as usize)
-        .fold((i - 1, 0i32), |(power, value), b| {
-
-            (power.saturating_sub(1), value + (*b as i32) * 10i32.pow(power))
-        });
-
-    Ok(out.1)
+    // There is no way for `neg` to be `Some(false)` so its fine.
+    Ok(v * neg.map(|_| -1).unwrap_or(1))
 }
 
 fn main() -> io::Result<()> {
