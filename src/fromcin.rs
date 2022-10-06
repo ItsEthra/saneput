9ac048d2d2f9fa3012202e6702b322511228a85c
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2022-10-06 12:30:57 +0300

    Added missing *size int types

--- src/fromcin.rs
+++ src/fromcin.rs
@@ -101,7 +101,7 @@ macro_rules! impl_from_cin_prim {
     };
 }
 
-impl_from_cin_prim!(i8, u8, i16, u16, i32, u32, i64, u64, i128, u128);
+impl_from_cin_prim!(i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize);
 
 impl FromStdin for f32 {
     type Error = ParseFloatError;
